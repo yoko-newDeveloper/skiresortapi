41c0014c500941b4544d3467ff5f4c82bb32ee48
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-11-09 14:23:22 +0900

    initial commit

---
+++ src/main/java/com/example/skiresortapi/SkiresortapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.skiresortapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiresortapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiresortapiApplication.class, args);
+	}
+
+}
