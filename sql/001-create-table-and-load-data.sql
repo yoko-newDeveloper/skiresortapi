4b6077f2813515acd16e306b97627fda2de4ad6c
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-11-18 21:12:18 +0900

    sql文の修正

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -7,6 +7,7 @@ CREATE TABLE skiresortapi (
   PRIMARY KEY(id)
 );
 
-INSERT INTO skiresortapi(id, name, area) VALUES (1, "niseko", "hokkaido");
-INSERT INTO skiresortapi(id, name, area) VALUES (2, "appi", "iwate");
-INSERT INTO skiresortapi(id, name, area) VALUES (3, "hakubahappoone", "nagano");
+INSERT INTO skiresortapi(id, name, area, impression) VALUES (1, "niseko", "hokkaido", "スノーボードデビューした広いゲレンデ");
+INSERT INTO skiresortapi(id, name, area, impression) VALUES (2, "appi", "iwate", "晴天率が低いので有名");
+INSERT INTO skiresortapi(id, name, area, impression) VALUES (3, "hakubahappoone", "nagano", "春の兎平はコブの聖地");
+INSERT INTO skiresortapi(id, name, area, impression) VALUES (4, "takamine", "nagano", "標高2000以上ですごく寒いけど雪質は良い");
