f456986b34fb350fddc5ae92da283d43b7925a6b
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-01-16 18:41:28 +0900

    存在するIDのデータを取得する

---
+++ NOTE/serviceTest.md
@@ -0,0 +1,37 @@
+- モック:偽物。本物のフリをする。
+- スタブ:代理。代わりのものを使う。
+
+## 考え方
+
+- ServiceがMapperに依存している状態なので、そのまま書くとエラー->単体テストなぼでMapperに依存できない
+- 依存しているクラスをモック化する
+- スタブ化(モック化)したMapperの状態を確認する
+- DBは無関係
+- doReturn:値を返すメソッドをスタブ化(モック化)した時に返す値を定義するためのメソッド
+
+  モックオブジェクトが特定のメソッド呼び出しに対して、特定の値を返すように指定するために使用される
+
+  モックオブジェクトがgetSomeValue()メソッドが呼ばれた時に42を返すように設定する例
+  `doReturn(42).when(someMock).getSomeValue();`
+
+## 準備
+
+- `@ExtendWith(MockitoExtension.class)`
+
+  -> Mockitoを使用できるようにclassに付ける
+- classに`@ExtendWith(MockitoExtension.class)` // JUnit5でMockitoを使うため
+- `@Mock` // モック化（スタブ化）する対象に定義
+- `@InjectMocks` // テスト対象に定義 @Mockでモックにしたインスタンスの注入先となるインスタンスに定義（インターフェースに付けるとエラー）
+
+## 実装
+
+- `doReturn`:Mapperの動作をスタブ化している仮のデータを定義している
+- `Skiresort actual`:実際の値が入る
+- `assertThat`:テスト対象の実行結果やオブジェクトの状態を期待する値や条件と比較する
+- `assertThat(actual)`:`assertThat`の引数`(actual)`に実際の値を定義する。Serviceが返した実際の値をassertThat(検証/比較)している
+- `assertThat(actual).isEqualTo(期待値となる値)`:期待値は`.isEqualTo`の引数に定義する
+- アサーションのimport文に気を付ける
+- 存在するidを指定した時、正常にデータが返されること
+    - `doReturn -when`:スタブ化した`id1`のデータを定義する
+    - `assertThat(actual).isEqualTo()`：`.isEqualTo`の引数に、期待値データを定義する
+    - `verify`：1回だけ`id1`が呼び出されたかを確認する
---
+++ src/test/java/com/example/skiresortapi/service/SkiresortServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.example.skiresortapi.service;
+
+import com.example.skiresortapi.entity.Skiresort;
+import com.example.skiresortapi.mapper.SkiresortMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class) // JUnit5でMpckitoを使うために必要
+class SkiresortServiceImplTest {
+
+    @InjectMocks // テスト対象
+    SkiresortServiceImpl skiresortServiceImpl;
+
+    @Mock // モック化(スタブ化)したいインスタンスに定義
+    SkiresortMapper skiresortMapper;
+
+    @Test
+    public void 存在するスキーリゾートのIDを指定した時に正常データが返されること() throws Exception {
+        // doReturn -when :Mokietoの記述
+        doReturn(Optional.of(new Skiresort(1, "zao", "yamagata", "樹氷が絶景で温泉も有名"))).when(skiresortMapper).findById(1);
+
+        // テストの実行
+        Skiresort actual = skiresortServiceImpl.findById(1);
+        assertThat(actual).isEqualTo(new Skiresort(1, "zao", "yamagata", "樹氷が絶景で温泉も有名"));
+        verify(skiresortMapper, times(1)).findById(1);
+
+    }
+
+}
