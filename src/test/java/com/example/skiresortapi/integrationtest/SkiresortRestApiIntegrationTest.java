5cf9dfc0b13acebea003069c0c89d2c8b7ffa228
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-03-10 09:24:36 +0900

    存在するIDを更新するテスト

--- src/test/java/com/example/skiresortapi/integrationtest/SkiresortRestApiIntegrationTest.java
+++ src/test/java/com/example/skiresortapi/integrationtest/SkiresortRestApiIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.skiresortapi.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -108,6 +109,34 @@ public class SkiresortRestApiIntegrationTest {
                         "area": "Canada"
                         }
                         """, response, JSONCompareMode.STRICT);
+        }
+    }
+
+    @Nested
+    class UpdateTest {
+        @Test
+        @DataSet(value = "datasets/it/skiresort.yml")
+        @ExpectedDataSet(value = "datasets/it/update-skiresort.yml")
+        @Transactional
+        void 存在するIDを指定してスキーリゾート情報を更新するとステータスコード200を返すこと() throws Exception {
+            String response = mockMvc.perform(MockMvcRequestBuilders.patch("/skiresorts/{id}", 3)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "id": 3,
+                                        "name": "Treble Cone",
+                                        "area": "NewZealand",
+                                        "impression": "Features a long course with views of Lake Wanaka"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+            JSONAssert.assertEquals("""
+                    {
+                        "message": "successfully update"
+                    }
+                    """, response, JSONCompareMode.STRICT);
         }
     }
 }
