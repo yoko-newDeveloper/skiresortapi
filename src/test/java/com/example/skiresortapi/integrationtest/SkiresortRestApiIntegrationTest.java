b9c5433b768059fb2ce21479f1000927a6b8e2fb
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-03-18 23:44:24 +0900

    CreateTestのみ実装

--- src/test/java/com/example/skiresortapi/integrationtest/SkiresortRestApiIntegrationTest.java
+++ src/test/java/com/example/skiresortapi/integrationtest/SkiresortRestApiIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.skiresortapi.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,29 +29,30 @@ public class SkiresortRestApiIntegrationTest {
     MockMvc mockMvc;
 
     @Nested
-    class ReadByIdTest {
+    class CreateTest {
         @Test
         @DataSet(value = "datasets/it/skiresort.yml")
+        @ExpectedDataSet(value = "datasets/it/create-skiresort.yml", ignoreCols = {"id"})
         @Transactional
-        void 存在するIDのスキーリゾートを取得した時ステータスコード200を返すこと() throws Exception {
-            String response = mockMvc.perform(MockMvcRequestBuilders.get("/skiresorts/{id}", 3))
-                    .andExpect(status().isOk())
+        void 新規のスキーリゾートを登録した時ステータスコード201を返すこと() throws Exception {
+            String response = mockMvc.perform(MockMvcRequestBuilders.post("/skiresorts")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "name": "Whistler",
+                                        "area": "Canada",
+                                        "impression": "Obtained Canadian snowboard instructor license"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isCreated())
                     .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
             JSONAssert.assertEquals("""
-                        {
-                            "name": "Zermatt",
-                            "area": "Swiss"
+                    {
+                        "name": "Whistler",
+                        "area": "Canada"
                         }
-                    """, response, JSONCompareMode.STRICT);
-        }
-
-        @Test
-        @DataSet(value = "datasets/it/skiresort.yml")
-        @Transactional
-        void 存在しないIDのスキーリゾートを取得した時ステータスコード404を返すこと() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.get("/skiresorts/{id}", 99))
-                    .andExpect(status().isNotFound());
+                        """, response, JSONCompareMode.STRICT);
         }
     }
 }
