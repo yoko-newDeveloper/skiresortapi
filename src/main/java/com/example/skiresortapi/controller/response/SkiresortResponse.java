f66e96843bea3190859298a387c7d2453c75150f
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2024-04-06 18:01:03 +0900

    SkiresortResponse:コメント追記

--- src/main/java/com/example/skiresortapi/controller/response/SkiresortResponse.java
+++ src/main/java/com/example/skiresortapi/controller/response/SkiresortResponse.java
@@ -2,7 +2,9 @@ package com.example.skiresortapi.controller.response;
 
 import com.example.skiresortapi.entity.Skiresort;
 
-// 個人情報などレスポンス内容を制限したい場合に設定するクラス
+/**
+ * 個人情報などレスポンス内容を制限したい場合に設定するクラス
+ */
 public class SkiresortResponse {
     private final String name;
     private final String area;
@@ -12,10 +14,20 @@ public class SkiresortResponse {
         this.area = skiresort.getArea();
     }
 
+    /**
+     * スキーリゾート名を取得する
+     *
+     * @return スキーリゾート名
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * スキーリゾートのエリアを取得する
+     *
+     * @return スキーリゾートエリア
+     */
     public String getArea() {
         return this.area;
     }
